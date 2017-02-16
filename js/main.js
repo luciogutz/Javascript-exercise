69f9d902edd1efb33cac01aef927ca1ebd2a3f4f
Lucio Gutierrez <lucio.gutierrez30@yahoo.com>
2017-02-16 10:11:47 -0600

    javascript exercises

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function(){
+  console.log("is this working");
+  var button = document.getElementById("button")
+  var Image = document.getElementById("image")
+
+
+    function addImage() {
+          var img = document.createElement("img");
+          img.src = "http://placeimg.com/640/480/nature";
+          document.child.appendChild(img);
+
+          return;
+  };
+
+});
